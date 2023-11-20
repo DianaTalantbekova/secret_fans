697851326cf84f6255669722fcd4c2a779c0c105
Ruslan Islanbekov <ruslan.islanbekov@dualbootpartners.com>
2023-11-20 23:11:29 +0600

    add integrate

--- lib/utils/links.dart
+++ lib/utils/links.dart
@@ -1,9 +1,9 @@
 class Links {
-  static const privacyPolicy = 'https://docs.google.com/document/d/1MXJQkbQja3tanW3W--s6o2qUQ7GWY9-QVbSVFDD_ZxA/edit?usp=sharing';
-  static const termsOfUse = 'https://docs.google.com/document/d/1kq8JrbrULvOU1dXnmzfLaIG3x0G7_eUGXfKGsn-KS_o/edit?usp=drive_link';
-  static const support = 'https://docs.google.com/forms/d/e/1FAIpQLSf0Vyil-5_JuIww5hfvrQ_TqzCFg0u7oi4ss4VmLYEMx9zFyw/viewform?usp=sharing';
+  static const privacyPolicy = '';
+  static const termsOfUse = '';
+  static const support = '';
 
-  static const apphud = 'app_XHYkZMDkUdPFCHZrGhfFzP2LyVHv5v';
+  static const apphud = '';
 
-  static const url = 'secretfansapi.site';
+  static const url = '';
 }
